b66e078d93daa2b025e0b5f4e9f34ebac0ce3dee
Khwezi Ndlovu <khwezindlovu25@gmail.com>
2024-01-29 00:18:12 -0800

    method named get_page that takes two integer arguments page with default value 1 and page_size with default value 10.

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 '''
-    Method get_hyper that takes the same arguments (and defaults) as get_page
-    and returns a dictionary containing the following key-value pairs.
+    Method named get_page that takes two integer arguments page
+    with default value 1 and page_size with default value 10.
 '''
 import csv
 import math
@@ -58,36 +58,3 @@ class Server:
             return []
         start, end = index_range(page, page_size)
         return self.dataset()[start:end]
-
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
-        '''
-            get_hyper: function
-            description: returns a page content depending on the page number
-                         and the page size
-            @self: Object constructor.
-            @page: Page number.
-            @page_size: the size of page.
-            return: A list containing the result of the specific page.
-        '''
-        data = self.get_page(page, page_size)
-        total_pages = math.ceil(len(self.dataset()) / page_size)
-        if page + 1 > total_pages:
-            next_page = None
-        else:
-            next_page = page + 1
-        if page - 1 < 1:
-            prev_page = None
-        else:
-            prev_page = page - 1
-        if len(data) == 0:
-            page_size_final = 0
-        else:
-            page_size_final = page_size
-        return {
-            'page_size': page_size_final,
-            'page': page,
-            'data': data,
-            'next_page': next_page,
-            'prev_page': prev_page,
-            'total_pages': total_pages,
-        }
