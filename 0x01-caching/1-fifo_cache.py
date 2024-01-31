abdff83348f2f74c7bd1444e4eabfd41ef8f5e0e
Khwezi Ndlovu <khwezindlovu25@gmail.com>
2024-01-31 00:00:24 -0800

    Create a class FIFOCache that inherits from BaseCaching and is a caching system

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+""" FIFOCache module
+"""
+
+from base_caching import BaseCaching
+
+class FIFOCache(BaseCaching):
+    """ FIFOCache inherits from BaseCaching and is a caching system using FIFO algorithm.
+    """
+    def __init__(self):
+        """ Initialize the FIFO cache.
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item to the cache using FIFO algorithm.
+        """
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                # Discard the first item (FIFO)
+                discarded_key = next(iter(self.cache_data))
+                del self.cache_data[discarded_key]
+                print("DISCARD: {}".format(discarded_key))
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item from the cache by key.
+        """
+        if key is not None:
+            return self.cache_data.get(key)
+
+# Driver code
+if __name__ == "__main__":
+    my_cache = FIFOCache()
+
+    my_cache.put("A", "Hello")
+    my_cache.put("B", "World")
+    my_cache.put("C", "Holberton")
+    my_cache.put("D", "School")
+    my_cache.print_cache()
+
+    my_cache.put("E", "Battery")
+    my_cache.print_cache()
+
+    my_cache.put("C", "Street")
+    my_cache.print_cache()
+
+    my_cache.put("F", "Mission")
+    my_cache.print_cache()
