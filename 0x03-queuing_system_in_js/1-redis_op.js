9cdc96012e6e7428929b353ae70f41ad6f2f8f83
Fumbesi <124091057+Fumbesi@users.noreply.github.com>
2024-03-08 12:51:22 +0200

    Create 1-redis_op.js

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+/**
+ * connecting to redis using node-redis
+ */
+import { createClient, print } from "redis";
+
+
+const client = createClient()
+
+client.on('error', (err) => {
+    console.log(`Redis client not connected to the server: ${err}`)
+});
+
+client.on('connect', () => {
+    console.log('Redis client connected to the server')
+})
+
+function setNewSchool(schoolName, value) {
+    client.SET(schoolName, value, print)
+}
+
+
+function displaySchoolValue(schoolName) {
+    client.GET(schoolName, (_err, val) => {
+        console.log(val);
+    })
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
