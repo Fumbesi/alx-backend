4a7e595fa1a1934d68996bceff689debc6b4d9b9
Fumbesi <124091057+Fumbesi@users.noreply.github.com>
2024-03-08 12:50:27 +0200

    Create 0-redis_client.js

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/**
+ * connecting to redis using node-redis
+ */
+import { createClient } from "redis";
+
+
+const client = createClient()
+
+client.on('error', (err) => {
+    console.log(`Redis client not connected to the server: ${err}`)
+});
+
+client.on('connect', () => {
+    console.log('Redis client connected to the server')
+})
